6f2e9461a4748317aac318e3f3f2678e9fe87405
陈志果 <chenzhiguo@chenzhiguodeMacBook-Pro.local>
2019-02-13 17:25:40 +0800

    文本消息自动回复

--- handle.py
+++ handle.py
@@ -2,6 +2,8 @@
 
 import hashlib
 import web
+import reply
+import receive
 
 class Handle(object):
     def GET(self):
@@ -17,10 +19,10 @@ class Handle(object):
             #公众号设置    
             token = "mytoken"
 
-            data_list = [token.encode("utf-8"),timestamp.encode("utf-8"),nonce.encode("utf-8")]
+            data_list = [token,timestamp,nonce]
             data_list.sort()
             sha1 = hashlib.sha1()
-            list(map(sha1.update,data_list))
+            list(map(sha1.update,list(map(lambda x : x.encode("utf-8"),data_list))))
             hashcode = sha1.hexdigest()
             print("[debug] handle/GET hashcode,signature:",hashcode,signature)
             if hashcode == signature:
@@ -30,3 +32,22 @@ class Handle(object):
 
         except Exception as Argument:
             return Argument        
+
+    def POST(self):
+        try:
+            webData = web.data()
+            print("[debug] post data",webData)
+
+            recMsg = receive.parse_xml(webData)
+            if isinstance(recMsg,receive.Msg) and recMsg.MsgType == 'text':
+                #需要check
+                toUser = recMsg.FromUserName
+                fromUser = recMsg.ToUserName
+                content = "test"
+                replyMsg = reply.TextMsg(toUser,fromUser,content)
+                return replyMsg.send()
+            else:
+                print("暂不处理")
+                return "success"
+        except Exception as Argument:
+            return Argument       
